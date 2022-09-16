18cc7ff0314e0a4f6215f951c6d190e18bae5f66
Sun Kit Tsui <sunkittsui@gmail.com>
2022-09-15 19:30:31 -0500

    Created GameEngine class
    Created GameEngine class and defined the fields
    and methods needed in this class with comments.

---
+++ src/main/java/GameEngine.java
@@ -0,0 +1,19 @@
+import java.util.Stack;
+
+public class GameEngine {
+    // FIELDS
+    // deque of cards to deal from (deal deque)
+    // players (2 - 10)
+        // each player has a hand of cards (more like a list)
+    // the discard pile
+
+    // METHODS
+    // determine number of players
+    // deal appropriate number of cards to each player
+    // play loop (use while true loop with index)
+    // reverse loop (reverse card)
+    // skip a player
+    // draw cards (for both when not having a card to put down or penalty)
+    // merging the discard pile into deal deque
+    // win condition checker
+}
