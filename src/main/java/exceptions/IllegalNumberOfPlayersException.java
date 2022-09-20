12ab9d34328a150fd27c7cf620b337c428398ced
Sun Kit Tsui <sunkittsui@gmail.com>
2022-09-20 11:06:03 -0500

    Commit IllegalNumberOfPlayersException

--- src/main/java/exceptions/IllegalNumberOfPlayersException.java
+++ src/main/java/exceptions/IllegalNumberOfPlayersException.java
@@ -1,2 +1,8 @@
-package exceptions;public class IllegalNumberOfPlayersException {
+package exceptions;
+
+public class IllegalNumberOfPlayersException extends RuntimeException{
+    public IllegalNumberOfPlayersException(int illegalNumberOfPlayers) {
+        super(illegalNumberOfPlayers < 2 ? "Min players: 2, Input: " + illegalNumberOfPlayers : "Max players: 10, Input: " + illegalNumberOfPlayers);
+    }
+
 }
